0fe243d38c5439910628d7d95a1e66f4961ed969
Morel-D <morelDen@gmail.com>
2023-02-17 12:20:38 +0100

    Usage of package provider

--- lib/Pages/wrapper.dart
+++ lib/Pages/wrapper.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:smoothie/Models/user.dart';
 import 'package:smoothie/Pages/Authentication/authenticate.dart';
 import 'package:smoothie/Pages/Home/home.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserModel?>(context);
+    print(user);
+
     return SafeArea(child: Authenticate());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:smoothie/Models/user.dart';
 import 'package:smoothie/Pages/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:smoothie/Services/auth.dart';
 import 'firebase_options.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +20,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return (MaterialApp(debugShowCheckedModeBanner: false, home: Wrapper()));
+    return StreamProvider<UserModel?>.value(
+      initialData: null,
+      value: AuthService().onAuthStateChanged,
+      builder: (context, snapshot) {
+        return MaterialApp(debugShowCheckedModeBanner: false, home: Wrapper());
+      },
+    );
   }
 }
